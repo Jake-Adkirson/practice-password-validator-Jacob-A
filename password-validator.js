01202d1c449444bdba152f11bd75e34d82451686
Jake Adkirson <jikjaklol363@gmail.com>
2025-03-23 18:13:27 -0500

    Built password validator to the practice criteria.

---
+++ password-validator.js
@@ -0,0 +1,10 @@
+const readline = require('readline-sync');
+let password;
+const minLength = 8;
+const hasUppercase = /[A-Z]/;
+const hasNumber = /[0-9]/;
+
+do{
+    password = readline.question("Enter your password\(Password must contain 8 characters, have at least 1 uppercase letter, and contain a number\):");
+} while (password.length < minLength || !hasUppercase.toLocaleString(password) || !hasNumber.toLocaleString(password));
+console.log("Password accepted")
