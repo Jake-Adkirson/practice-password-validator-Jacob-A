80ac44e6398e81e788a2ed7e3ca55564bcff3213
Jake Adkirson <jikjaklol363@gmail.com>
2025-03-23 18:20:27 -0500

    Added message asking you to retry if criteria wasn't met.

--- password-validator.js
+++ password-validator.js
@@ -6,5 +6,8 @@ const hasNumber = /[0-9]/;
 
 do{
     password = readline.question("Enter your password\(Password must contain 8 characters, have at least 1 uppercase letter, and contain a number\):");
+    if (password.length < minLength || !hasUppercase.toLocaleString(password) || !hasNumber.toLocaleString(password)) {
+        console.log("Please enter a different password");
+    }
 } while (password.length < minLength || !hasUppercase.toLocaleString(password) || !hasNumber.toLocaleString(password));
 console.log("Password accepted")
